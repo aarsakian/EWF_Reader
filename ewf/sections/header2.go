71f30c653c984d022470a42e58b54bc605a09fda
aarsakian <aarsakian@gmail.com>
2023-04-28 12:34:46 -0700

    kept header2 only methods

--- ewf/sections/header2.go
+++ ewf/sections/header2.go
@@ -10,34 +10,6 @@ import (
 	"github.com/aarsakian/EWF_Reader/ewf/utils"
 )
 
-const (
-	EWF_HEADER_VALUES_INDEX_DESCRIPTION = iota
-	EWF_HEADER_VALUES_INDEX_CASE_NUMBER
-	EWF_HEADER_VALUES_INDEX_EXAMINER_NAME
-	EWF_HEADER_VALUES_INDEX_EVIDENCE_NUMBER
-	EWF_HEADER_VALUES_INDEX_NOTES
-	EWF_HEADER_VALUES_INDEX_ACQUIRY_SOFTWARE_VERSION
-	EWF_HEADER_VALUES_INDEX_ACQUIRY_OPERATING_SYSTEM
-	EWF_HEADER_VALUES_INDEX_ACQUIRY_DATE
-	EWF_HEADER_VALUES_INDEX_SYSTEM_DATE
-	EWF_HEADER_VALUES_INDEX_PASSWORD
-	EWF_HEADER_VALUES_INDEX_PROCESS_IDENTIFIER
-	EWF_HEADER_VALUES_INDEX_UNKNOWN_DC
-	EWF_HEADER_VALUES_INDEX_EXTENTS
-	EWF_HEADER_VALUES_INDEX_COMPRESSION_TYPE
-
-	EWF_HEADER_VALUES_INDEX_MODEL
-	EWF_HEADER_VALUES_INDEX_SERIAL_NUMBER
-	EWF_HEADER_VALUES_INDEX_DEVICE_LABEL
-
-	/* Value to indicate the default number of header values
-	 */
-	EWF_HEADER_VALUES_DEFAULT_AMOUNT
-)
-
-type EWF_Header_Section struct {
-}
-
 type EWF_Header2_Section struct {
 	BOM           [2]uint8 //declares endianess 0xff0xfe (little) vice versa
 	NofCategories uint16
@@ -55,10 +27,6 @@ type EWF_Header2_Section struct {
 	pid           string    "Process Identifiers"
 	dc            string    "Unknown"
 	ext           string    "Extents"
-}
-
-func (ewf_header_section *EWF_Header_Section) Parse(buf []byte) {
-
 }
 
 func (ewf_h2_section *EWF_Header2_Section) Parse(buf []byte) {
@@ -105,10 +73,10 @@ func (ewf_h2_section *EWF_Header2_Section) Parse(buf []byte) {
 				} else if id_num == EWF_HEADER_VALUES_INDEX_ACQUIRY_OPERATING_SYSTEM {
 					ewf_h2_section.ov = string(attr)
 				} else if id_num == EWF_HEADER_VALUES_INDEX_ACQUIRY_DATE {
-					ewf_h2_section.m = utils.SetTime(attr)
+					ewf_h2_section.m = utils.GetTime(attr)
 
 				} else if id_num == EWF_HEADER_VALUES_INDEX_SYSTEM_DATE {
-					ewf_h2_section.u = utils.SetTime(attr)
+					ewf_h2_section.u = utils.GetTime(attr)
 
 				} else if id_num == EWF_HEADER_VALUES_INDEX_PASSWORD {
 					ewf_h2_section.p = string(attr)
@@ -123,24 +91,6 @@ func (ewf_h2_section *EWF_Header2_Section) Parse(buf []byte) {
 
 }
 
-func NeedsDecompress(section_obj interface{}) bool {
-	var needsDecompress bool
-	switch section_obj.(type) {
-
-	case *EWF_Header2_Section:
-		needsDecompress = true
-	case *EWF_Header_Section:
-		needsDecompress = true
-	default:
-		needsDecompress = false
-	}
-	return needsDecompress
-}
-
-func (ewf_h2_section *EWF_Header2_Section) GetAttr(string) interface{} {
-	return ""
-}
-
-func (ewf_h_section *EWF_Header_Section) GetAttr(string) interface{} {
-	return ""
+func (ewf_h2_section EWF_Header2_Section) GetAttr(string) interface{} {
+	return ewf_h2_section
 }
