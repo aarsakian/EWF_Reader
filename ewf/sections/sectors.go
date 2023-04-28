13ee15ac72db7a9565c24ee78b6c1ab268fe548f
aarsakian <aarsakian@gmail.com>
2023-04-28 12:36:43 -0700

    updated sectors

--- ewf/sections/sectors.go
+++ ewf/sections/sectors.go
@@ -8,12 +8,17 @@ import (
 	//     "hash/adler32"
 )
 
+type DataChuck struct {
+	data     []byte
+	Checksum [4]uint8 //adler-32
+}
+
 type EWF_Sectors_Section struct {
-	body []byte
+	DataChucks []DataChuck
 }
 
 func (ewf_sectors_section *EWF_Sectors_Section) GetAttr(string) interface{} {
-	return ewf_sectors_section.body[:]
+	return ewf_sectors_section.DataChucks
 }
 
 /*func (ewf_sectors_section *EWF_Sectors_Section) Verify() bool {
@@ -23,7 +28,7 @@ func (ewf_sectors_section *EWF_Sectors_Section) GetAttr(string) interface{} {
 
 func (ewf_sectors_section *EWF_Sectors_Section) Parse(buf []byte) {
 
-	utils.Unmarshal(buf, ewf_sectors_section.body)
+	utils.Unmarshal(buf, ewf_sectors_section)
 
 	//  fmt.Println("ERR FREE",ewf_sectors_section.Verify())
 }
