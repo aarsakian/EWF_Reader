9de2632eade60f3e8f62c83b1ff59f18c5919e6a
Armen Arsakian <aarsakian@gmail.com>
2023-05-28 22:20:18 +0300

    testing functions

--- main.go
+++ main.go
@@ -70,13 +70,13 @@ func main() {
 	var ewf_image ewf.EWF_Image
 	ewf_image.ParseEvidence(filenames) //consumer
 	ewf_image.GetChunckOffsets()
-	ewf_image.ReadAt(48423, 512)
+	//ewf_image.ReadAt(48423, 512)
 	ewf_image.ShowInfo()
 	hash := ewf_image.GetHash()
 
 	fmt.Println(hash)
-	verified := ewf_image.Verify()
-	ewf_image.VerifyHash()
+	//	verified := ewf_image.Verify()
+	verified := ewf_image.VerifyHash()
 	fmt.Println("Verified status", verified)
 
 }
